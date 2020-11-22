1ebeb2c5cbf244059f7eb871fa70ad02ca17a5f7
cxh1378 <985838836@qq.com>
2020-11-22 16:06:10 +0800

    init

---
+++ server.js
@@ -0,0 +1,51 @@
+/*
+ *真辉聊 v0.0.1
+ *cxh1378 November 22, 2020
+ *MIT license
+ *view on GitHub:https://github.com/cxh1378/zhenhuiliao
+ *see it in action:http://www.sosoduo.com/23333
+ */
+
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  users = []
+//specify the html we will use
+app.use('/', express.static(__dirname + '/www'))
+//bind the server to the 80 port
+//server.listen(3000);//for local test
+server.listen(process.env.PORT || 23333) //publish to heroku
+//server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
+//console.log('server started on port'+process.env.PORT || 3000);
+//handle the socket
+io.sockets.on('connection', function (socket) {
+  //new user login
+  socket.on('login', function (nickname) {
+    if (users.indexOf(nickname) > -1) {
+      socket.emit('nickExisted')
+    } else {
+      //socket.userIndex = users.length;
+      socket.nickname = nickname
+      users.push(nickname)
+      socket.emit('loginSuccess')
+      io.sockets.emit('system', nickname, users.length, 'login')
+    }
+  })
+  //user leaves
+  socket.on('disconnect', function () {
+    if (socket.nickname != null) {
+      //users.splice(socket.userIndex, 1);
+      users.splice(users.indexOf(socket.nickname), 1)
+      socket.broadcast.emit('system', socket.nickname, users.length, 'logout')
+    }
+  })
+  //new message get
+  socket.on('postMsg', function (msg, color) {
+    socket.broadcast.emit('newMsg', socket.nickname, msg, color)
+  })
+  //new image get
+  socket.on('img', function (imgData, color) {
+    socket.broadcast.emit('newImg', socket.nickname, imgData, color)
+  })
+})
